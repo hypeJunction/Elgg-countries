b2568cb832239e5f588da03ec9d3530fb5bbda69
nlybe <nikos@lyberakis.gr>
2017-01-15 00:43:55 +0200

    compatibility with profile_manager plugin, option to set default country, Elgg 2.x compatibility

--- views/default/input/country.php
+++ views/default/input/country.php
@@ -7,17 +7,19 @@
 $vars['options_values'] = elgg_get_countries();
 
 if (isset($vars['class'])) {
-	$vars['class'] = "{$vars['class']} elgg-input-country";
+    $vars['class'] = "{$vars['class']} elgg-input-country";
 } else {
-	$vars['class'] = 'elgg-input-country';
+    $vars['class'] = 'elgg-input-country';
 }
 
 if (!isset($vars['name'])) {
-	$vars['name'] = 'country';
+    $vars['name'] = 'country';
 }
 
 if (!isset($vars['value']) || !$vars['value']) {
     $vars['value'] = elgg_get_default_country();
 }
 
-echo elgg_view('input/dropdown', $vars);
+//echo elgg_view('input/dropdown', $vars);
+
+echo elgg_view_input('dropdown', $vars);
