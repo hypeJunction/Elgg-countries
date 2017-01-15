80376642dbf81410ddf321597e3e2a4519a619d0
nlybe <nikos@lyberakis.gr>
2017-01-15 19:35:15 +0200

    small code improvements

--- views/default/plugins/countries/settings.php
+++ views/default/plugins/countries/settings.php
@@ -11,7 +11,7 @@ array_unshift($countries , elgg_echo('countries:settings:select_country'));
 
 $basic_settings = elgg_format_element('div', [], elgg_view_input('dropdown', array(
     'name' => 'params[default_country]',
-    'value' => ($plugin->default_country?$plugin->default_country:elgg_get_default_country()),
+    'value' => ($plugin->default_country?$plugin->default_country:''),
     'options_values' => $countries,
     'label' => elgg_echo('countries:settings:default_country'),
     'help' => elgg_echo('countries:settings:default_country:help'),
